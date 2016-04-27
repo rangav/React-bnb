d0d952acadd19062c2d65ba272245ce2708081e6
Alex Kotliarskyi <alex.frantic@gmail.com>
2016-04-27 16:36:29 -0700

    Add Listing flow type definition

---
+++ js/data.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+export type Listing = {
+  id: number;
+  name: string;
+  instantBook: boolean;
+  price: number;
+  starRating: number;
+  reviewsCount: number;
+  previewImageURL: string;
+};
